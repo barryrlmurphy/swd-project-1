e1d57b41356e04269c317772e018db1002c8157e
bmurphy <barry@donedeal.ie>
2016-09-07 23:00:28 +0100

    added validation for account. added account storage.

--- app/scripts/controllers/accountController.js
+++ app/scripts/controllers/accountController.js
@@ -8,17 +8,42 @@
  * Controller of the project1App
  */
 angular.module('project1App')
-  .controller('AccountCtrl', ['$scope', function ($scope) {
+  .controller('AccountCtrl', ['$scope','localStorageService', function ($scope,localStorageService) {
 
     console.log('project1App!');
   	console.log($scope);
+  	var thisClass = this;
 
 	$scope.submitForm = function(isValid) {
 		console.log('form submit: ' + isValid);
 		if (isValid) {
-			console.log('our form is amazing');
+			console.log('our form is valid :)');
+			thisClass.saveAccount();
+		}else{
+			console.log('form is not valid');
 		}
 
+	};
+
+	$scope.comparePasswords = function(){
+		if($scope.newAccountForm.password.$viewValue !== $scope.newAccountForm.repeatPassword.$viewValue){
+			$scope.newAccountForm.repeatPassword.$invalid = true;	
+		}else{
+			$scope.newAccountForm.repeatPassword.$invalid = false;
+		}
+	};
+
+	this.saveAccount = function(){
+		var accountDetails = {
+			'name' : $scope.newAccountForm.name.$viewValue,
+			'email' : $scope.newAccountForm.email.$viewValue,
+			'password' : $scope.newAccountForm.password.$viewValue 
+		};
+		localStorageService.set('accountDetails', accountDetails);
+
+		var retreivedAccount = localStorageService.get('accountDetails');
+		console.log('saved account: ');
+		console.log(retreivedAccount);
 	};
 
   }]);
